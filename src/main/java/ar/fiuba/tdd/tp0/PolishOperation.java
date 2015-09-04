8ca4873fb13d600320458de021296b00d164d211
Xero-Hige <gaston.martinez.90@gmail.com>
2015-09-04 13:25:11 -0300

    Add base operation interface Add parse expresion loop

---
+++ src/main/java/ar/fiuba/tdd/tp0/PolishOperation.java
@@ -0,0 +1,10 @@
+package ar.fiuba.tdd.tp0;
+
+import java.util.Stack;
+
+/**
+ * Created by hige on 30/08/15.
+ */
+public interface PolishOperation {
+    static default void solve(Stack<String> args);
+}
--- src/main/java/ar/fiuba/tdd/tp0/RPNCalculator.java
+++ src/main/java/ar/fiuba/tdd/tp0/RPNCalculator.java
@@ -1,9 +1,24 @@
 package ar.fiuba.tdd.tp0;
 
+import java.util.Hashtable;
+
 public class RPNCalculator {
 
+    private Hashtable<String,PolishOperation> operations;
+
+    private boolean isInt(String value){                //FIXME
+        return String.matches("-?\\d+(\\.\\d+)?");
+    }
+
+    private RPNCalculator(){
+        operations = new Hashtable<String,PolishOperation>();
+    }
+
     public float eval(String expression) {
-        throw new RuntimeException("not implemented yet!");
+        String[] tokens = expression.split(" ");
+        for (String token:tokens){
+
+        }
     }
 
 }
